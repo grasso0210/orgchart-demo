5d22ee02b4de36911b43b0a4758c11e857e01ce9
Futoshi TASAKI <grasso@grasso.homeip.net>
2020-04-23 21:27:02 +0900

    tutorial vue-orgchart

---
+++ src/views/Chart1.vue
@@ -0,0 +1,33 @@
+<template>
+  <vo-basic 
+    :data="chartData"
+    :pan="true"
+    :zoom="true"
+  >
+  </vo-basic>
+</template>
+
+<script>
+import { VoBasic } from 'vue-orgchart'
+
+export default {
+  components: { VoBasic },
+
+  created() {
+    this.chartData = {
+      name: 'JavaScript',
+      children: [
+        { name: 'Angular' },
+        {
+          name: 'React',
+          children: [{ name: 'Preact' }],
+        },
+        {
+          name: 'Vue',
+          children: [{ name: 'Moon' }],
+        },
+      ],
+    }
+  },
+}
+</script>
